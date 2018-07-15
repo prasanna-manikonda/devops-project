e134492559e924fe53f542458cf8ebb03efa3769
Administrator <Administrator@172.30.0.131>
2018-07-15 15:55:02 +0000

    Commit to local repo

---
+++ javamaven/src/main/java/com/devops/javamaven/App.java
@@ -0,0 +1,13 @@
+package com.devops.javamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
